957e572b52efe8b2495ff8cd7fb3ee73d6cbba01
Sarshar Imtiaz <sarshar.samoo@gmail.com>
2026-01-19 02:22:58 +0500

    Changes

---
+++ DevOps-Dotnet/sb-cancel/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Messaging.ServiceBus;
+
+var conn = "<Your_SB_Connnection_String>";
+var queue = "examplequeuename";
+long seqToCancel = <Your_Seq_Number>;
+
+await using var client = new ServiceBusClient(conn);
+ServiceBusSender sender = client.CreateSender(queue);
+
+await sender.CancelScheduledMessageAsync(seqToCancel);
+
+Console.WriteLine($"Cancelled scheduled message seq={seqToCancel}");
